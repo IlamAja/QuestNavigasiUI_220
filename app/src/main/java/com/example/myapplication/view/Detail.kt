45052e2cedae980d6dad15a35e2b7ecb7a2d2aaa
Ilam Ball <informasiilam@gmail.com>
2025-11-06 19:38:50 +0700

    Menambahkan `TopAppBar` pada halaman detail

--- app/src/main/java/com/example/myapplication/view/Detail.kt
+++ app/src/main/java/com/example/myapplication/view/Detail.kt
@@ -2,9 +2,12 @@ package com.example.myapplication.view
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.myapplication.R
+
 
 
 @Composable
@@ -16,4 +19,13 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(text = stringResource(id=R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }){     isiRuang->
+        Column
+    }
 }
