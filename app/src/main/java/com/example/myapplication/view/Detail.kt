024aac594c7a7432844c0b6434f08c587d09637a
Ilam Ball <informasiilam@gmail.com>
2025-11-06 19:34:46 +0700

    membuat file Detail.kt

--- app/src/main/java/com/example/myapplication/view/Detail.kt
+++ app/src/main/java/com/example/myapplication/view/Detail.kt
@@ -1,2 +1,19 @@
 package com.example.myapplication.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.myapplication.R
+
+
+@Composable
+fun TampilData(
+    onBackBtnClick:() -> Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+}
