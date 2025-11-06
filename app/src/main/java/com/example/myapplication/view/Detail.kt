f31a5bbf0273be6fcbccfa9622907e8219eb5cfb
Ilam Ball <informasiilam@gmail.com>
2025-11-06 19:49:08 +0700

    Menambahkan anotasi @OptIn(ExperimentalMaterial3Api::class)

--- app/src/main/java/com/example/myapplication/view/Detail.kt
+++ app/src/main/java/com/example/myapplication/view/Detail.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,7 +26,7 @@ import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
 
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:() -> Unit
